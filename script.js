870b6db54d6b9927c5a0549ea6ccfca6a0bfc17d
yihfei <lim.yih.fei@gmail.com>
2024-06-24 15:45:57 +0800

    Add calculator logic

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const clearButton = document.querySelector('.clear');
+const negativeButton = document.querySelector('.negative');
+const percentageButton = document.querySelector('.percentage');
+const numbers = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.operator');
+const display = document.querySelector('.display');
+
+let evaluate;
+
 function add(a, b) {
     return a + b;
 }
@@ -14,17 +23,98 @@ function divide(a, b) {
     return a / b;
 }
 
-function operate(operator, a, b) {
+
+
+function operate(operator, a) {
     switch(operator) {
         case "+":
-            return add(a, b);
+            return b => add(a, b);
         case "-":
-            return subtract(a, b);
-        case "*":
-            return multiply(a, b);
+            return b => subtract(a, b);
+        case "x":
+            return b => multiply(a, b);
         case "/":
-            return divide(a, b);
+            return b => divide(a, b);
+        case "=":
+            display.textContent = a;
+            return ;
         default:
             return "error";
     }
 }
+
+function inputNumber(e) {
+    if (clearOperator() || evaluate) {
+        display.textContent = '';
+    }
+    
+    const input = e.target.textContent;
+    const lastChar = display.textContent.charAt(display.textContent.length - 1);
+
+    let isNegative = false;
+
+    if (display.textContent.includes('-')) {
+        isNegative = true;
+        display.textContent = display.textContent.slice(1);
+    }
+
+    if (input == '.' && lastChar != '.') {
+        display.textContent += input;
+    } else if (input != '.' && display.textContent.includes('.')) {
+        display.textContent += input;
+    } else if (input != '.') {
+        display.textContent = Number(display.textContent) * 10 + Number(input);
+    }
+
+    if (isNegative) {
+        display.textContent = '-' + display.textContent;
+    }
+
+}
+
+function inputOperator(e) {
+    clearOperator()
+    const operator = (e.target.textContent);
+    currentNumber = Number(display.textContent);
+
+    if (evaluate) {
+        evaluate = evaluate(currentNumber);
+    } else {
+        evaluate = currentNumber
+    }
+
+    //display.textContent = '';
+    e.target.classList.add('selected');
+
+    evaluate = operate(operator, evaluate);
+
+
+}
+
+function clearOperator() {
+    let isEqual = false;
+    if (document.querySelector('.equal').classList.contains('selected')) {
+        isEqual = true;
+    }
+
+    operators.forEach(operator => {
+        operator.classList.remove('selected');
+    })
+
+    return isEqual;
+}
+
+clearButton.addEventListener('click', () => {
+    display.textContent = '0';
+});
+
+negativeButton.addEventListener('click', () => {
+    if (display.textContent.includes('-')) {
+        return;
+    }
+    display.textContent = '-' + display.textContent;
+})
+
+
+numbers.forEach(number => number.addEventListener('click', inputNumber));
+operators.forEach(operator => operator.addEventListener('click', inputOperator));
